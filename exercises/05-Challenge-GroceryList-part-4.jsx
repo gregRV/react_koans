9228816b3d1fba915254b8c136a7413580e398c0
Greg RV <gregvarias@gmail.com>
2015-11-08 21:58:33 -0800

    Complete all exercises (aside from extra credit).

--- exercises/05-Challenge-GroceryList-part-4.jsx
+++ exercises/05-Challenge-GroceryList-part-4.jsx
@@ -75,7 +75,15 @@ class GroceryList extends React.Component {
   // Fill the definition of the following method to allow completing each item
   // Hint 1: Pay attention to the element's index on the list.
   toggleGroceryCompleteness(groceryIndex) {
-    // Put your code here
+    let newState = this.state.groceries.map(function(item, index){
+      if (groceryIndex === index) {
+        item.completed = true;
+      }
+      return item;
+    });
+    this.setState({
+      groceries: newState
+    });
   }
 
   render() {
